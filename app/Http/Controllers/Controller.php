f90ad34fa6ba2a13de0ab16f1da3b320c0fdd6a0
OfKiNgHeRo <luongduc1246>
2016-04-28 20:17:41 +0700

    UPdate category

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,5 +7,18 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
-
+        public function getFileName($file)
+        {
+            $name =$file->getClientOriginalName(); 
+            $fileName =rand(0,99999).$name; 
+            return $fileName;
+        }
+        public function saveFile($file,$destinationPath,$fileName)
+        {
+            $file->move($destinationPath, $fileName);
+        }
+        public function back()
+        {
+            return redirect()->back();
+        }
 }
